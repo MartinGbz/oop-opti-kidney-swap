2c58ba12951e2aa7640aa945d62be90dc9299d6d
SebastienBLT <sebastien.bulte@ig2i.centralelille.fr>
2022-06-15 19:46:43 +0200

    Modification URL fichier json (retour dans l'app kidney) car sinon obligation de disposer de l'api web sinon erreur

--- src/test/TestAllSolveur.java
+++ src/test/TestAllSolveur.java
@@ -241,7 +241,7 @@ public class TestAllSolveur{
         FileWriter ecritureJson = null;
         try {
             ecriture = new PrintWriter(nomFichierResultats+".csv");
-            ecritureJson = new FileWriter("../kidney-webapp/src/app/"+nomFichierResultats+".json");
+            ecritureJson = new FileWriter("./"+nomFichierResultats+".json");
             printEnTetes(ecriture);
 
             //Fabrication du json & csv pour l'ensemble des instances
